b1ac90442874d54bb66d8d95abf39e343f4fd6a0
jp <acarvalho.jp@gmail.com>
2025-05-05 01:12:34 +0100

    feat: Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/home"
+import { Login } from "./pages/login"
 
 function App() {
 
-  return (
-    <>
-      <Home></Home>
-    </>
-  )
+  	return (
+    	<BrowserRouter>
+			<Routes>
+				<Route path="/login" element={<Login />}></Route>
+			</Routes>
+    	</BrowserRouter>
+  	)
 }
 
 export default App
